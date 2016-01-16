77a1ffab1fade3df0bbe7c4e1465d64a9afc3c4d
macadamc <macadamchris@gmail.com>
2016-01-16 17:59:38 -0500

    Camera & Camera Zones
    basic camera and camera zones implementation

---
+++ Assets/CameraController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform camTarget;
+    public float targetSize;
+    public float moveSpd;
+    public float zoomSpd;
+
+    private static bool cameraExists;
+    public Vector3 targetPos;
+    private Camera cam;
+
+    public float horExtent;                                // 1/2 the horizontal size of the camera.
+    public float verExtent;                                // 1/2 the vertical size of the camera.
+
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    // Use this for initialization
+    void Start () {
+
+        DontDestroyOnLoad(transform.gameObject);
+
+        if (!cameraExists)
+        {
+            cameraExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        cam = GetComponent<Camera>();
+
+        verExtent = cam.orthographicSize;
+        horExtent = verExtent * Screen.width / Screen.height;
+
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        if(cam.orthographicSize != targetSize)
+        {
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetSize, zoomSpd*Time.deltaTime);
+
+            verExtent = cam.orthographicSize;
+            horExtent = verExtent * Screen.width / Screen.height;
+        }
+
+        targetPos = new Vector3(camTarget.transform.position.x, camTarget.transform.position.y, -10);
+        targetPos.x = Mathf.Clamp(targetPos.x, minX+horExtent, maxX-horExtent);
+        targetPos.y = Mathf.Clamp(targetPos.y, minY+verExtent, maxY-verExtent);
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, moveSpd * Time.deltaTime);
+
+    }
+
+    public void SetMinMax(float miX,float maX,float miY,float maY)
+    {
+        minX = miX;
+        maxX = maX;
+        minY = miY;
+        maxY = maY;
+    }
+    public void SetBaseOrthoSize(float size)
+    {
+        targetSize = size;
+    }
+}
---
+++ Assets/CameraZone.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZone : MonoBehaviour {
+
+    public float camOrthoSize;
+
+    private CameraController cam;
+
+    private float minX;
+    private float maxX;
+    private float minY;
+    private float maxY;
+
+    private BoxCollider2D camZone;
+
+
+	// Use this for initialization
+	void Start () {
+        cam = FindObjectOfType<CameraController>();
+
+        camZone = GetComponent<BoxCollider2D>();
+
+        minX = transform.position.x - (camZone.size.x / 2);
+        maxX = transform.position.x + (camZone.size.x / 2);
+        minY = transform.position.y - (camZone.size.y / 2);
+        maxY = transform.position.y + (camZone.size.y / 2);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            cam.SetMinMax(minX,maxX,minY,maxY);
+            cam.SetBaseOrthoSize(camOrthoSize);
+        }
+    }
+}
