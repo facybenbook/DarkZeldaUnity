6c467e5f8dd762b740fb98b11457f07eaca02284
DoucheBagMIKE <mikemac440@gmail.com>
2016-01-16 21:49:50 -0500

    Player Attacking

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : ScriptableObject {
+
+	public virtual void Use()
+    {
+        Debug.Log("Base Item Called.");
+    }
+}
---
+++ Assets/Scripts/Items/Sword.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sword : Item {
+
+    Animator playerAnimator;
+    public int damage;
+
+    void OnEnable()
+    {
+        name = "Sword";
+        damage = 5;
+        this.playerAnimator = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+    }
+
+    public override void Use()
+    {
+        //base.Use();
+        Debug.Log(name + " Used.");
+        playerAnimator.SetTrigger("isAttacking");
+    }
+}
